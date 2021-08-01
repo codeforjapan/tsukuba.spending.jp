47f326f68840847e6813830223ac141ba88e1f9f
osoken <osoken.devel@outlook.jp>
2021-08-01 11:16:42 +0900

    closes #7

--- js/setting.js
+++ js/setting.js
@@ -10,146 +10,220 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
+OpenSpending.identifier = 'tsukubashi_budget';
+OpenSpending.year = '2020';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
-  /* yokohama cofog */
   '1': {
-    icon: 'icons/helping-others.svg',
+    icon: 'icons/admin-culture.svg',
     color: '#418fe2',
     bcolor: '#935B3B'
   },
   '1-1': {
-    icon: 'icons/helping-others.svg',
+    icon: 'icons/sports.svg',
+    color: '#418fe2',
+    bcolor: '#935B3B'
+  },
+  '1-2': {
+    icon: 'icons/island.svg',
+    color: '#418fe2',
+    bcolor: '#935B3B'
+  },
+  '1-3': {
+    icon: 'icons/culture.svg',
     color: '#418fe2',
     bcolor: '#935B3B'
   },
   '2': {
-    icon: 'icons/schools.svg',
+    icon: 'icons/police.svg',
     color: '#418fe2',
     bcolor: '#0AB971'
   },
   '2-1': {
-    icon: 'icons/pre-school.svg',
+    icon: 'icons/ambulance.svg',
     color: '#418fe2',
     bcolor: '#0AB971'
   },
   '2-2': {
-    icon: 'icons/education.svg',
+    icon: 'icons/communication.svg',
     color: '#418fe2',
     bcolor: '#0AB971'
   },
   '3': {
-    icon: 'icons/economy-tourism.svg',
+    icon: 'icons/environment-admin.svg',
     color: '#418fe2',
     bcolor: '#4E6D00'
   },
   '3-1': {
-    icon: 'icons/dollar.svg',
+    icon: 'icons/garbage.svg',
     color: '#418fe2',
     bcolor: '#4E6D00'
   },
   '3-2': {
-    icon: 'icons/harbor.svg',
+    icon: 'icons/toilet.svg',
     color: '#418fe2',
     bcolor: '#4E6D00'
   },
   '3-3': {
-    icon: 'icons/culture.svg',
+    icon: 'icons/forest.svg',
+    color: '#418fe2',
+    bcolor: '#4E6D00'
+  },
+  '3-4': {
+    icon: 'icons/pig.svg',
+    color: '#418fe2',
+    bcolor: '#4E6D00'
+  },
+  '3-5': {
+    icon: 'icons/pollution.svg',
     color: '#418fe2',
     bcolor: '#4E6D00'
   },
   '4': {
-    icon: 'icons/our-streets.svg',
+    icon: 'icons/schools.svg',
     color: '#418fe2',
     bcolor: '#D33673'
   },
   '4-1': {
-    icon: 'icons/farms.svg',
+    icon: 'icons/education.svg',
     color: '#418fe2',
     bcolor: '#D33673'
   },
   '4-2': {
-    icon: 'icons/street-lights.svg',
+    icon: 'icons/secondary-lower.svg',
     color: '#418fe2',
     bcolor: '#D33673'
   },
   '4-3': {
-    icon: 'icons/housing.svg',
+    icon: 'icons/microscope.svg',
+    color: '#418fe2',
+    bcolor: '#D33673'
+  },
+  '4-4': {
+    icon: 'icons/secondary-upper.svg',
+    color: '#418fe2',
+    bcolor: '#D33673'
+  },
+  '4-5': {
+    icon: 'icons/research.svg',
+    color: '#418fe2',
+    bcolor: '#D33673'
+  },
+  '4-6': {
+    icon: 'icons/pre-school.svg',
     color: '#418fe2',
     bcolor: '#D33673'
   },
   '5': {
-    icon: 'icons/environment.svg',
+    icon: 'icons/economy-tourism.svg',
     color: '#418fe2',
     bcolor: '#2A3A03'
   },
   '5-1': {
-    icon: 'icons/tree.svg',
+    icon: 'icons/manufactoring-construction.svg',
     color: '#418fe2',
     bcolor: '#2A3A03'
   },
   '5-2': {
-    icon: 'icons/wind.svg',
+    icon: 'icons/economic-aid.svg',
+    color: '#418fe2',
+    bcolor: '#2A3A03'
+  },
+  '5-3': {
+    icon: 'icons/planning.svg',
+    color: '#418fe2',
+    bcolor: '#2A3A03'
+  },
+  '5-4': {
+    icon: 'icons/unemployment.svg',
+    color: '#418fe2',
+    bcolor: '#2A3A03'
+  },
+  '5-5': {
+    icon: 'icons/transport.svg',
+    color: '#418fe2',
+    bcolor: '#2A3A03'
+  },
+  '5-6': {
+    icon: 'icons/farms.svg',
     color: '#418fe2',
     bcolor: '#2A3A03'
   },
   '6': {
-    icon: 'icons/civilian-action.svg',
+    icon: 'icons/community.svg',
     color: '#418fe2',
     bcolor: '#EC2406'
   },
   '6-1': {
-    icon: 'icons/civilian-action.svg',
+    icon: 'icons/family2.svg',
+    color: '#418fe2',
+    bcolor: '#EC2406'
+  },
+  '6-2': {
+    icon: 'icons/street-lights.svg',
+    color: '#418fe2',
+    bcolor: '#EC2406'
+  },
+  '6-3': {
+    icon: 'icons/housing.svg',
     color: '#418fe2',
     bcolor: '#EC2406'
   },
   '7': {
-    icon: 'icons/traffic-watersup.svg',
+    icon: 'icons/publicaffairs.svg',
     color: '#418fe2',
     bcolor: '#938626'
   },
   '7-1': {
-    icon: 'icons/traffic-watersup.svg',
+    icon: 'icons/publicaffairs.svg',
     color: '#418fe2',
     bcolor: '#938626'
   },
   '8': {
-    icon: 'icons/government.svg',
+    icon: 'icons/wheelchair.svg',
     color: '#418fe2',
     bcolor: '#418fe2'
   },
   '8-1': {
-    icon: 'icons/publicaffairs.svg',
+    icon: 'icons/family.svg',
     color: '#418fe2',
     bcolor: '#418fe2'
   },
   '8-2': {
-    icon: 'icons/government.svg',
+    icon: 'icons/old-age.svg',
+    color: '#418fe2',
+    bcolor: '#418fe2'
+  },
+  '8-3': {
+    icon: 'icons/civilian-action.svg',
+    color: '#418fe2',
+    bcolor: '#418fe2'
+  },
+  '8-4': {
+    icon: 'icons/helping-others.svg',
+    color: '#418fe2',
+    bcolor: '#418fe2'
+  },
+  '8-5': {
+    icon: 'icons/medical-supplies.svg',
     color: '#418fe2',
     bcolor: '#418fe2'
   },
   '9': {
-    icon: 'icons/garbage.svg',
+    icon: 'icons/health.svg',
     color: '#418fe2',
     bcolor: '#D33673'
   },
   '9-1': {
-    icon: 'icons/garbage.svg',
+    icon: 'icons/hospital.svg',
     color: '#418fe2',
     bcolor: '#D33673'
   },
-  '10': {
-    icon: 'icons/order-safety.svg',
+  '9-2': {
+    icon: 'icons/water.svg',
     color: '#418fe2',
-    bcolor: '#790586'
-  },
-  '10-1': {
-    icon: 'icons/order-safety.svg',
-    color: '#418fe2',
-    bcolor: '#790586'
+    bcolor: '#D33673'
   }
 };
